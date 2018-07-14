f38bf120de54d67ef996c5faa24728c9a1c81165
bxlkm <bxlkm@qq.com>
2018-07-14 14:28:02 +0800

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  walletUrl: 'https://www.onechain.one',
+  votingUrl: 'http://app.onechain.one/appstart.html',
+  appName: 'SBC BlockChain',
+  logoUrl: '/assets/logo.png',
+  apiUrl: 'http://block.sbc.one:8008',
+  blockchainUrl: 'http://222.180.164.139:18000'
+};
